efc82f3881eb5333bb3c501a879b91b46e2c3ae9
Yana_N <122021948+YanaNozhnina@users.noreply.github.com>
2023-06-13 09:01:05 +0500

    change pages

--- try_pages.py
+++ try_pages.py
@@ -11,14 +11,14 @@ show_pages(
     [
         Page("example_app/streamlit_app.py", "Home", "🏠"),
         # Can use :<icon-name>: or the actual icon
-        Page("example_app/example_one.py", "Example One", ":books:"),
+        Page("example_app/metal.py", "Metal", "📎"),
         # The pages appear in the order you pass them
-        Page("example_app/example_four.py", "Example Four", "📖"),
-        Page("example_app/example_two.py", "Example Two", "✏️"),
+        Page("example_app/plastic.py", "Plastic", "♲"),
+        Page("example_app/paper.py", "Paper", "📰"),
         # Will use the default icon and name based on the filename if you don't
         # pass them
-        Page("example_app/example_three.py"),
-        Page("example_app/example_five.py", "Example Five", "🧰"),
+        Page("example_app/glass.py", "Glass", "🥛"),
+        Page("example_app/other.py", "Other", "🗑"),
     ]
 )
 
