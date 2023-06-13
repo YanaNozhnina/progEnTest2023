c7d30183cec5fcb09b24aaafa886c0fa1d905a23
Yana_N <122021948+YanaNozhnina@users.noreply.github.com>
2023-06-13 15:27:43 +0500

    changes of easy_disp

--- example_app/easy_disposal.py
+++ example_app/easy_disposal.py
@@ -1,17 +1,72 @@
 import streamlit as st
 
-st.header("Proper disposal is easy! (Yekaterinburg)")
+
+st.header("Proper disposal is easy!")
 st.subheader("Rule No.1 Hazardous waste must be handed over")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
+   st.image("pictures/Рисунок1.jpg")
+   st.write("•	Stores: M video, Eldorado, Leroy Merlin, Lenta.")
 with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
+   st.image("pictures/Рисунок2.png")
+   st.image("pictures/Рисунок3.jpg")
+   st.write("•	CBM of Complex solution of industrial waste problems \
+            (Engels St., 16, Alpinist St., 57 A).")
+   st.write("•	Industrial Waste Safety Center")
 with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+   st.image("pictures/Рисунок4.jpg")
+   st.write("•	The company «Spetsavtokom».")
+   st.write("•	The company LLC «TD Uralneftekhim» (Monterskaya str., 8).")
+   st.write("•	ECOPROM LLC (Kosareva str., 91a).")
+
+
+st.subheader("Rule No.2 Do not throw away clothes, shoes, toys, textiles, books")
+col4, col5, col6, col7, col8 = st.columns(5)
+
+with col4:
+   st.image("pictures/Рисунок5.jpg")
+with col5:
+   st.image("pictures/Рисунок6.jpg")
+with col6:
+   st.image("pictures/Рисунок7.jpg")
+with col7:
+   st.image("pictures/Рисунок8.png")
+with col8:
+   st.image("pictures/Рисунок9.jpg")
+
+st.write("•	Industrial Charity organization «Aistenok»\
+          (Moskovskaya str., 25A).")
+st.write("•	The Center for social assistance to families \
+         and children «Karavella» (Moskovsky trakt str., 8 km, 8)")
+st.write("•	Social project «The Thing of Good» \
+         (Tatishcheva str., 90; Sheinkman str., 90; 8 Marta str., 149)")
+
+
+st.subheader("Rule No.3 Sort the waste and hand it over for recycling")
+col9, col10, col11, col12 = st.columns(4)
+with col9:
+   st.image("pictures/Рисунок10.jpg")
+with col10:
+   st.image("pictures/Рисунок11.jpg")
+with col11:
+   st.image("pictures/Рисунок12.jpg")
+with col12:
+   st.image("pictures/Рисунок13.jpg")
+
+st.write("•	Industrial Charity organization «Aistenok» (Moskovskaya str., 25A).")
+st.write("•	The Center for social assistance to families \
+         and children «Karavella» (Moskovsky trakt str., 8 km, 8)")
+st.write("•	Social project «The Thing of Good» \
+         (Tatishcheva str., 90; Sheinkman str., 90; 8 Marta str., 149)")
+st.write("•	Charity clothing resale «Blagomarket» (Boris Yeltsin str., 3)")
+
+
+st.subheader("Rule No.4 Reduce the amount of household garbage")
+col9, col10, col11, col12 = st.columns(4)
+with col9:  
+
+
+#«» 
+
+
