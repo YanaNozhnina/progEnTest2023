5b8773612a8869d24c1e6ff2523e1f4c9c42c432
Yana_N <122021948+YanaNozhnina@users.noreply.github.com>
2023-06-13 18:47:17 +0500

    add header

--- try_pages.py
+++ try_pages.py
@@ -5,8 +5,9 @@ import numpy as np
 
 from st_pages import Page, add_page_title, show_pages
 
+st.header("Responsible person's page :writing_hand:")
 
-st.write("More and more people recycle everyday.")
+st.subheader("More and more people recycle everyday.")
 st.write("This helps to reduce the need for landfill and more costly forms of disposal.\
           Recycling also reduces the need for extracting (mining, quarrying and logging),\
           refining and processing raw materials all of which create substantial air and water pollution.")
