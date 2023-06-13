fd987a96cccd0f9f4bbd23a3ab775a5ff423c26f
Yana_N <122021948+YanaNozhnina@users.noreply.github.com>
2023-06-13 10:11:26 +0500

    add new pages

---
+++ example_app/paper.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+from st_pages import add_page_title
+
+add_page_title("Your papers, please!")
+
+st.write("Paper makes up 23 percent of municipal solid waste (trash) \
+         generated each year, more than any other material. \
+         Americans recycled about 68 percent of the paper they used in 2018. \
+         This recovered paper is used to make new paper products, \
+         which saves trees and other natural resources. \
+         Most community or office recycling programs accept paper and paper products. \
+         Check what your community or office program accepts before you put it in the bin. \
+         Look for products that are made from recycled paper when you shop. \
+         Better yet, consider if you really need to print in the first place.")
+
+st.subheader("Can I recycle newspapers?")
+st.write("Yes, newspapers can be recycled.")
+st.subheader("Can I recycle magazines?")
+st.write("Yes, magazines can be recycled. ")
+st.subheader("Can I recycle pizza boxes? ")
+st.write("Yes! Pizza boxes can be recycled, even if they have grease in them.\
+          Make sure to remove any food scraps from the box and flatten it before placing it in the bin.")
+
+
+
--- try_pages.py
+++ try_pages.py
@@ -10,14 +10,10 @@ from st_pages import Page, add_page_title, show_pages
 show_pages(
     [
         Page("example_app/streamlit_app.py", "Home", "🏠"),
-        # Can use :<icon-name>: or the actual icon
-        Page("example_app/metal.py", "Metal", "📎"),
-        # The pages appear in the order you pass them
-        Page("example_app/plastic.py", "Plastic", "♲"),
-        Page("example_app/paper.py", "Paper", "📰"),
-        # Will use the default icon and name based on the filename if you don't
-        # pass them
         Page("example_app/glass.py", "Glass", "🥛"),
+        Page("example_app/metal.py", "Metal", "📎"),
+        Page("example_app/paper.py", "Paper", "📰"),
+        Page("example_app/plastic.py", "Plastic", "♲"),
         Page("example_app/other.py", "Other", "🗑"),
     ]
 )
